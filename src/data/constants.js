e0715061681b01213316fc8b0ef29bb98e89fc45
chandusiriyala <chandusiriyala116@gmail.com>
2024-12-03 16:21:05 +0530

    Taskify Project added

--- src/data/constants.js
+++ src/data/constants.js
@@ -454,28 +454,23 @@ export const projects = [
   },
   {
     id: 9,
-    title: "Trackify",
-    date: "Jun 2023 - Jul 2023",
+    title: "Taskify",
+    date: "Oct 2024 - Oct 2024",
     description:
-      "Trackify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
+      "Taskify is a web application designed to streamline task management and enhance productivity in the workplace. It provides a user-friendly interface for employers to keep track of their employees' daily work activities and empowers employees to log their tasks efficiently. \nAdmin Credentials: # Email: testadmin@gmail.com #Password- 123@testadmin, Employee Credentials:	#Email: testemployee@gmail.com	#Password- 123@Testemployee",
     image:
       "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
     tags: [
-      "Docker",
-      "AWS",
-      "DuckDNS",
-      "Eslint",
-      "Husky",
-      "CI/CD",
       "React Js",
       "MongoDb",
       "Node Js",
       "Express Js",
       "Redux",
+      "Next JS"
     ],
     category: "web app",
-    github: "https://github.com/chandusiriyala7",
-    webapp: "https://trackify-management.netlify.app",
+    github: "https://github.com/chandusiriyala7/taskify",
+    webapp: "https://taskify-black-theta.vercel.app/",
   },
 ];
 
